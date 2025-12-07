cc1850f823b21f6e76a7ad9eb5e66dced58385ab
Daniel Yoann <64357912+F4IGV@users.noreply.github.com>
2025-12-07 11:55:07 +0100

    Update run_dapnet_solar.bat with folder comment
    Added comment to specify folder containing the .py file.

--- run_dapnet_solar.bat
+++ run_dapnet_solar.bat
@@ -1,4 +1,5 @@
 @echo off
 REM Lance le script Python pour envoyer la météo solaire sur DAPNET
-cd /d "C:\Scripts\DAPNET"
+cd /d "C:\Scripts\DAPNET"  #choose your folder who contain the .py file
+
 py "dapnet_solar_pocsag.py" >> "C:\Scripts\DAPNET\dapnet_solar.log" 2>&1
